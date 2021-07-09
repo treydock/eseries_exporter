e1f145f0699e2f1efa10916a3b77042e431cf4d6
Trey Dockendorf <tdockendorf@osc.edu>
2021-07-09 14:20:44 -0400

    Avoid errors if duplicate drives are encountered

--- collector/drives_test.go
+++ collector/drives_test.go
@@ -98,6 +98,69 @@ func TestDrivesCollector(t *testing.T) {
 	}
 }
 
+func TestDrivesCollectorDuplicates(t *testing.T) {
+	fixtureData, err := os.ReadFile("testdata/drives-duplicate.json")
+	if err != nil {
+		t.Fatalf("Error loading fixture data: %s", err.Error())
+	}
+	expected := `
+	# HELP eseries_drive_status Drive status
+	# TYPE eseries_drive_status gauge
+	eseries_drive_status{slot="53",status="bypassed",tray="0"} 0
+	eseries_drive_status{slot="53",status="dataRelocation",tray="0"} 0
+	eseries_drive_status{slot="53",status="failed",tray="0"} 1
+	eseries_drive_status{slot="53",status="incompatible",tray="0"} 0
+	eseries_drive_status{slot="53",status="optimal",tray="0"} 0
+	eseries_drive_status{slot="53",status="preFailCopy",tray="0"} 0
+	eseries_drive_status{slot="53",status="preFailCopyPending",tray="0"} 0
+	eseries_drive_status{slot="53",status="removed",tray="0"} 0
+	eseries_drive_status{slot="53",status="replaced",tray="0"} 0
+	eseries_drive_status{slot="53",status="unknown",tray="0"} 0
+	eseries_drive_status{slot="53",status="unresponsive",tray="0"} 0
+	eseries_drive_status{slot="53",status="__UNDEFINED",tray="0"} 0
+	eseries_drive_status{slot="58",status="bypassed",tray="0"} 0
+	eseries_drive_status{slot="58",status="dataRelocation",tray="0"} 0
+	eseries_drive_status{slot="58",status="failed",tray="0"} 0
+	eseries_drive_status{slot="58",status="incompatible",tray="0"} 0
+	eseries_drive_status{slot="58",status="optimal",tray="0"} 1
+	eseries_drive_status{slot="58",status="preFailCopy",tray="0"} 0
+	eseries_drive_status{slot="58",status="preFailCopyPending",tray="0"} 0
+	eseries_drive_status{slot="58",status="removed",tray="0"} 0
+	eseries_drive_status{slot="58",status="replaced",tray="0"} 0
+	eseries_drive_status{slot="58",status="unknown",tray="0"} 0
+	eseries_drive_status{slot="58",status="unresponsive",tray="0"} 0
+	eseries_drive_status{slot="58",status="__UNDEFINED",tray="0"} 0
+	# HELP eseries_exporter_collect_error Indicates if error has occurred during collection
+	# TYPE eseries_exporter_collect_error gauge
+	eseries_exporter_collect_error{collector="drives"} 1
+	`
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		_, _ = rw.Write(fixtureData)
+	}))
+	defer server.Close()
+	baseURL, _ := url.Parse(server.URL)
+	target := config.Target{
+		Name:       "test",
+		User:       "test",
+		Password:   "test",
+		BaseURL:    baseURL,
+		HttpClient: &http.Client{},
+	}
+	w := log.NewSyncWriter(os.Stderr)
+	logger := log.NewLogfmtLogger(w)
+	collector := NewDrivesExporter(target, logger)
+	gatherers := setupGatherer(collector)
+	if val, err := testutil.GatherAndCount(gatherers); err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	} else if val != 26 {
+		t.Errorf("Unexpected collection count %d, expected 26", val)
+	}
+	if err := testutil.GatherAndCompare(gatherers, strings.NewReader(expected),
+		"eseries_drive_status", "eseries_exporter_collect_error"); err != nil {
+		t.Errorf("unexpected collecting result:\n%s", err)
+	}
+}
+
 func TestDrivesCollectorError(t *testing.T) {
 	expected := `
 	# HELP eseries_exporter_collect_error Indicates if error has occurred during collection
